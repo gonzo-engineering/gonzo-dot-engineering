a995b091776f0d52aed5f4076ee8cff0d721484d
Frederick O'Brien <frederik.obrien@gmail.com>
2024-06-22 00:19:57 +0100

    Barebones placeholder site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>01100111 01101111 01101110 01111010 01101111</title>
+</svelte:head>
+
+<h1>01100111 01101111 01101110 01111010 01101111</h1>
+
+<style>
+	h1 {
+		position: absolute;
+		text-align: center;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		font-family: 'Courier New', Courier, monospace;
+		color: lightgray;
+	}
+</style>
