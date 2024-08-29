562c4017c4714636e3df8b98992f948ddbed2ede
Frederick O'Brien <frederik.obrien@gmail.com>
2024-08-29 01:03:13 +0100

    Replace binary with glitchy link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,8 @@
 <script>
+	import '$lib/styles/reset.css';
 	import '$lib/styles/global.css';
 </script>
 
 <main>
 	<slot />
 </main>
-
-<style>
-	main {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-		color: lightgray;
-		background-color: #1a1a1a;
-	}
-</style>
