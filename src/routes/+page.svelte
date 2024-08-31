d1a77d1182835a32048fa019191f141a8175130c
Frederick O'Brien <frederik.obrien@gmail.com>
2024-08-31 21:13:23 +0100

    Flesh out homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,237 +3,63 @@
 	<meta name="description" content="Freak technology." />
 </svelte:head>
 
-<div class="glitch-wrapper">
-	<div class="glitch" data-glitch="glitch">
-		<a href="https://github.com/gonzo-engineering">freak technology</a>
-	</div>
-</div>
+<main>
+	<p>The web is decedent and depraved.</p>
+	<p>
+		Gonzo Engineering is a project devoted to building free, open source tools for the benefit of
+		journalists, musicians, and other members of the freak kingdom.
+	</p>
+	<p>Ventures so far:</p>
+	<ul>
+		<li>
+			<a href="https://teeline.online">teeline.online</a>, an interactive learning site for Teeline
+			shorthand
+		</li>
+		<li>
+			<a href="https://github.com/gonzo-engineering/soli">Soli</a>, a 'just add water' toolkit for
+			immersive musician websites
+		</li>
+	</ul>
+	<p>
+		Support the development of these tools and more by <a
+			href="https://opencollective.com/gonzo-engineering">becoming a backer on Open Collective</a
+		>. It all adds up.
+	</p>
+	<p>
+		Want to collaborate? <a href="https://github.com/gonzo-engineering/">Join me on GitHub</a>
+		or
+		<a href="mailto:frederik.obrien@gmail.com">get in touch</a>.
+	</p>
+</main>
 
 <style>
+	main {
+		padding: 1em;
+		/* text-transform: lowercase; */
+		text-shadow: 0 0 5px #c8c8c8;
+	}
 	a {
-		color: lightgray;
-		text-decoration: none;
+		font-weight: bold;
 	}
 	a:hover {
-		color: white;
-		text-decoration: underline;
+		opacity: 0.8;
 	}
-	.glitch-wrapper {
-		position: absolute;
-		text-align: center;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		color: lightgray;
-		text-shadow:
-			0 0 5px #b2b2b2,
-			0 0 10px #fff;
-		white-space: nowrap;
+	ul {
+		list-style-type: none;
+		padding-left: 0;
+		margin-bottom: 1em;
 	}
-
-	.glitch {
-		position: relative;
-		font-size: 1.4rem;
-		font-weight: 700;
-		line-height: 1.2;
-		color: #fff;
-		letter-spacing: 5px;
-		z-index: 1;
+	ul li::before {
+		content: '>';
+		margin-right: 0.5em;
 	}
-
-	.glitch:before {
-		content: attr(data-glitch);
-		position: absolute;
-		top: 0;
-		left: -2px;
-		width: 100%;
-		color: #fff;
-		overflow: hidden;
-		clip: rect(0, 900px, 0, 0);
-		animation: noise-before 3s infinite linear alternate-reverse;
-	}
-
-	.glitch:after {
-		content: attr(data-glitch);
-		position: absolute;
-		top: 0;
-		left: 2px;
-		width: 100%;
-		color: #fff;
-		overflow: hidden;
-		clip: rect(0, 900px, 0, 0);
-		animation: noise-after 2s infinite linear alternate-reverse;
-	}
-
-	@keyframes noise-before {
-		0% {
-			clip: rect(61px, 9999px, 52px, 0);
-		}
-
-		5% {
-			clip: rect(33px, 9999px, 144px, 0);
-		}
-
-		10% {
-			clip: rect(121px, 9999px, 115px, 0);
-		}
-
-		15% {
-			clip: rect(144px, 9999px, 162px, 0);
-		}
-
-		20% {
-			clip: rect(62px, 9999px, 180px, 0);
-		}
-
-		25% {
-			clip: rect(34px, 9999px, 42px, 0);
-		}
-
-		30% {
-			clip: rect(147px, 9999px, 179px, 0);
-		}
-
-		35% {
-			clip: rect(99px, 9999px, 63px, 0);
-		}
-
-		40% {
-			clip: rect(188px, 9999px, 122px, 0);
-		}
-
-		45% {
-			clip: rect(154px, 9999px, 14px, 0);
-		}
-
-		50% {
-			clip: rect(63px, 9999px, 37px, 0);
-		}
-
-		55% {
-			clip: rect(161px, 9999px, 147px, 0);
-		}
-
-		60% {
-			clip: rect(109px, 9999px, 175px, 0);
-		}
-
-		65% {
-			clip: rect(157px, 9999px, 88px, 0);
-		}
-
-		70% {
-			clip: rect(173px, 9999px, 131px, 0);
-		}
-
-		75% {
-			clip: rect(62px, 9999px, 70px, 0);
-		}
-
-		80% {
-			clip: rect(24px, 9999px, 153px, 0);
-		}
-
-		85% {
-			clip: rect(138px, 9999px, 40px, 0);
-		}
-
-		90% {
-			clip: rect(79px, 9999px, 136px, 0);
-		}
-
-		95% {
-			clip: rect(25px, 9999px, 34px, 0);
-		}
-
-		100% {
-			clip: rect(173px, 9999px, 166px, 0);
-		}
-	}
-
-	@keyframes noise-after {
-		0% {
-			clip: rect(26px, 9999px, 33px, 0);
-		}
-
-		5% {
-			clip: rect(140px, 9999px, 198px, 0);
-		}
-
-		10% {
-			clip: rect(184px, 9999px, 89px, 0);
-		}
-
-		15% {
-			clip: rect(121px, 9999px, 6px, 0);
-		}
-
-		20% {
-			clip: rect(181px, 9999px, 99px, 0);
-		}
-
-		25% {
-			clip: rect(154px, 9999px, 133px, 0);
-		}
-
-		30% {
-			clip: rect(134px, 9999px, 169px, 0);
-		}
-
-		35% {
-			clip: rect(26px, 9999px, 187px, 0);
-		}
-
-		40% {
-			clip: rect(147px, 9999px, 137px, 0);
-		}
-
-		45% {
-			clip: rect(31px, 9999px, 52px, 0);
-		}
-
-		50% {
-			clip: rect(191px, 9999px, 109px, 0);
-		}
-
-		55% {
-			clip: rect(74px, 9999px, 54px, 0);
-		}
-
-		60% {
-			clip: rect(145px, 9999px, 75px, 0);
-		}
-
-		65% {
-			clip: rect(153px, 9999px, 198px, 0);
-		}
-
-		70% {
-			clip: rect(99px, 9999px, 136px, 0);
-		}
-
-		75% {
-			clip: rect(118px, 9999px, 192px, 0);
-		}
-
-		80% {
-			clip: rect(1px, 9999px, 83px, 0);
-		}
-
-		85% {
-			clip: rect(145px, 9999px, 98px, 0);
-		}
-
-		90% {
-			clip: rect(121px, 9999px, 154px, 0);
-		}
-
-		95% {
-			clip: rect(156px, 9999px, 44px, 0);
-		}
-
-		100% {
-			clip: rect(67px, 9999px, 122px, 0);
+	@media (min-width: 768px) {
+		main {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			max-width: 800px;
 		}
 	}
 </style>
