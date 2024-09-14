e8d9a18a79069d00c4286d5196993bce305ea479
Frederick O'Brien <frederik.obrien@gmail.com>
2024-09-14 01:09:18 +0100

    Simplify screen styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script>
 	import '$lib/styles/reset.css';
 	import '$lib/styles/global.css';
-	import Screen from '$lib/components/Screen.svelte';
 </script>
 
-<Screen>
-	<main>
-		<slot />
-	</main>
-</Screen>
+<slot />
